9e613e5d16969bc53dd00566ba8e198d694c9bbe
Jan Van Bouwel <jan.van.bouwel@proton.me>
2024-05-01 01:33:54 +0200

    Add temporary node initialiser

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { setTimeout } from "node:timers/promises";
 import { zts } from "./module/zts";
 
 /**
@@ -7,12 +8,40 @@ import { zts } from "./module/zts";
  */
 export function startNode(
   path?: string,
-  eventHandler?: (event: number) => void,
+  eventHandler?: (event: number) => void
 ) {
   if (path) zts.init_from_storage(path);
   if (eventHandler) zts.init_set_event_handler(eventHandler);
 
   zts.node_start();
+}
+
+/**
+ * Starts zt node, joins a network and returns ipv6 or else ipv4 address.
+ * @param path 
+ * @param nwid 
+ * @param eventHandler 
+ * @returns 
+ */
+export async function startNodeAndJoinNet(
+  path: string | undefined,
+  nwid: string,
+  eventHandler?: (event: number) => void
+): Promise<string> {
+  startNode(path, eventHandler);
+  while (!zts.node_is_online()) {
+    await setTimeout(50);
+  }
+  zts.net_join(nwid);
+  while (!zts.net_transport_is_ready(nwid)) {
+    await setTimeout(50);
+  }
+
+  try {
+    return zts.addr_get_str(nwid, true)
+  } catch (error) {
+    return zts.addr_get_str(nwid, false)
+  }
 }
 
 export * from "./module/zts";
